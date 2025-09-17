9ecc97da587fa4a022347fedb1ccb116ff0b2a15
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-17 09:32:23 +0200

    python

---
+++ Actividades Guiadas y mas/Actividad2Guiada.py
@@ -0,0 +1,7 @@
+var1=4
+var4=6
+print (f"la suma de {var1} + {var4} =",var1+var4 )
+var5=int(input("introduce un numero "))
+var6=int(input("introduce otro numero "))
+total= var5+var6
+print (" el resultado es ",total)
---
+++ Ejercicios iniciales Python/Ejercicio2.py
@@ -0,0 +1,4 @@
+var1=int(input("introduce un valor "))
+var2=(input("introduce un valor "))
+var3=int(input("introduce un valor "))
+print(f"los valores introducidos son {var1}, {var2}, {var3}")
