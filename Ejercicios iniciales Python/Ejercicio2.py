929aed1ab6aad8fe0246267b5e21d4cabb804f48
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-17 09:43:34 +0200

    Update Ejercicio2.py

--- Ejercicios iniciales Python/Ejercicio2.py
+++ Ejercicios iniciales Python/Ejercicio2.py
@@ -1,4 +1,4 @@
-var1=(input("introduce un valor "))
+var1=int(input("introduce un valor "))
 var2=(input("introduce un valor "))
-var3=(input("introduce un valor "))
+var3=float(input("introduce un valor "))
 print(f"los valores introducidos son {var1}, {var2}, {var3}.")
