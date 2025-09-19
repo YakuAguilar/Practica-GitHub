0840d02bbcc85869563221799b35a83daeff0ec2
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-19 15:47:30 +0200

    python

---
+++ Ejercicios iniciales Python/Ejercicio8.py
@@ -0,0 +1,6 @@
+var1=float(input("introduce catidad de horas "))
+var2=60
+var3=3600
+totalminutos=var1*var2
+totalsegundos=var1*var3
+print(f"{var1} son {totalminutos} minutos y {totalsegundos} segundos")
