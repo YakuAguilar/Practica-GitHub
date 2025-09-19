1b2b89d3bc2e3822a15603c34b3c2701eb1f4ee7
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-19 15:24:36 +0200

    Update Ejercicio7.py

---
+++ Ejercicios iniciales Python/Ejercicio7.py
@@ -0,0 +1,23 @@
+var1=int(input("introduce un número:"))
+var2=int(input("introduce otro número:"))
+#suma= +
+total1=var1+var2
+#resta= -
+total2=var1-var2
+#multiplicacion= *
+total3=var1*var2
+#division= /
+total4=var1/var2
+#potencia=**
+total5=var1**var2
+#división con resultado de número entero=//
+total6=var1//var2
+#modulo=%
+total7=var1%var2
+print(f"la suma es:",total1)
+print(f"la resta es:", total2)
+print(f"la multiplicación es:", total3)
+print(f"la división es:", total4)
+print(f"la potencia es:", total5)
+print(f"la división con resultado de número entero es:", total6)
+print(f"el modulo entre operadores es:", total7)
