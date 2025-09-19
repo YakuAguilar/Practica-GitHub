917cef2004a3aa4fb2b3261329f024b1bfafeeb4
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-19 15:31:07 +0200

    Update Ejercicio5.py

---
+++ Ejercicios iniciales Python/Ejercicio5.py
@@ -0,0 +1,6 @@
+var1=(input("introduce una palabra "))
+var2=(input("introduce otro palabra "))
+var3=(input("introduce otro palabra "))
+var4=(input("introduce otro palabra "))
+var5=(input("introduce otro palabra "))
+print(F"{var1}, {var2}, {var3}, {var4}, {var5}.")
