01120cce296862766148f80e5828db89495e971f
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-23 10:22:56 +0200

    Update Ejercicio11.py

---
+++ Ejercicios iniciales Python/Ejercicio11.py
@@ -0,0 +1,5 @@
+#Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro.
+var1=int(input("introduce la medida de un lado del cuadrado "))
+print(f"el area es {var1*var1}")
+print(f"el perimetro es {var1*4}")
+
