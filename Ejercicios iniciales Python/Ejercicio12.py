b93ea6ec34887802d49a785381057d08fe623002
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-23 10:36:40 +0200

    Update Ejercicio12.py

---
+++ Ejercicios iniciales Python/Ejercicio12.py
@@ -0,0 +1,8 @@
+#Realiza un programa que, introduciendo en los valores de lado, base menor, base mayor y altura de un trapecio isósceles, nos devuelva por pantalla en el área y el perímetro.
+var4=float(input("introduce los valores de lado "))
+var1=float(input("introduce la base menor "))
+var2=float(input("introduce la base mayor "))
+var3=float(input("introduce la altura "))
+
+print(f"el area del trapecio isósceles es {(var3*(var1+var2))/2}")
+print(f"el perimetro del trapecio isósceles es {var1+var2+(var4*2)}")
