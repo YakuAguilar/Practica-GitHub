64b2c8b81bc625e13e89a396924df0046761199e
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-26 15:26:24 +0200

    Update Ejercicio13.py

--- Ejercicios iniciales Python/Ejercicio13.py
+++ Ejercicios iniciales Python/Ejercicio13.py
@@ -1,4 +1,4 @@
 # Realiza un programa que, a partir introducir el lado de un cubo, presente por pantalla el área y para calcular el volumen utiliza el operador de exponente.
 var1=float(input(f"introduce la medida de un lado "))
-print(f"el area es {var1*var1*var1}")
-print(f"el perimetro es ")
+print(f"el area es {6*(var1**2)}")
+print(f"el volumen es {var1**3}")
