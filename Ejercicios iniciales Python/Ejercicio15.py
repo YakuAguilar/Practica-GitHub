3dda56751e4d32cca5dbed26510045962c9e7fa8
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-26 15:53:48 +0200

    Update Ejercicio15.py

--- Ejercicios iniciales Python/Ejercicio15.py
+++ Ejercicios iniciales Python/Ejercicio15.py
@@ -1,1 +1,8 @@
 # Utiliza el valor Pi de la librería math para calcular el área y volumen de un cilindro, introduciendo por teclado el valor de radio y altura. Resultado con 2 decimales.
+import math
+var1=float(input("introduce la altura de un cilindro "))
+var2=float(input("introduce el radio de un cilindro "))
+redondeo1=round(((2*math.pi*(var2**2))+2*math.pi*var2*var1), 2)
+print(f"el area del circulo es {redondeo1}")
+redondeo2=round(math.pi*(var2**2)*var1, 2)
+print(f"el volumen del circulo es {redondeo2}")
