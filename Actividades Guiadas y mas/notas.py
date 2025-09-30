2cdf7ba8261e1902a3b1bcbf3fe5dc5abceaa8c7
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-30 10:34:56 +0200

    Update notas.py

--- Actividades Guiadas y mas/notas.py
+++ Actividades Guiadas y mas/notas.py
@@ -6,3 +6,19 @@
 #%	Realiza un módulo entre los operandos	16 % 3 = 1
 #**	Realiza la potencia de los operandos	12 ** 3 = 1728
 #//	Realiza la división con resultado de número entero	18 // 5 = 3
+print("       menu    ")
+print("1. hamburguesa")
+print("2. pizza")
+print("3. lentejas")
+print("4. bocadillo de jamon")
+
+opcion=int(input("intoduce el numero del menu "))
+
+if opcion==1:
+    print("has escogido hamburguesa")
+if opcion==2:
+    print("has escogido pizza")
+if opcion==3:
+    print("has ecogido lentejas")
+if opcion==4:
+    print("has escogido bocadillo de jamon ")
