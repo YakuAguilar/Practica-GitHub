f1f3d37a9245545275c47461cfbf266d363e709d
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-09-30 10:20:51 +0200

    python

--- Ejercicios iniciales Python/Ejercicios iniciales python.py
+++
@@ -1,6 +0,0 @@
-#Utiliza el método sqrt de la librería math para calcular la raíz cuadrada de un número. El resultado de la raíz cuadrada divídelo entre 2 de manera que se obtenga siempre un resultado entero. Haz que se muestre por pantalla los dos resultados de todo el proceso(raíz y división)
-import math
-var1=float(input("introduce un numero"))
-raizcuadrada=round(math.sqrt(var1), 1)
-print(f"la raiz cuadrada es {raizcuadrada}")
-print(f" el resultado de la division es {raizcuadrada}")
