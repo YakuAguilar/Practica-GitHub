66d50649137d5052bc67ec036da49f4c260459b2
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-10-03 16:01:22 +0200

    Update Ejercicio32.py

--- Ejercicios Python 2 (condicionales)/Ejercicio32.py
+++ Ejercicios Python 2 (condicionales)/Ejercicio32.py
@@ -1,12 +1,1 @@
-
-
-var1=(input("introduce una letra "))
-
-
-
-if var1.isupper():
-     print("la letra introducida es mayuscula")
-elif var1.islower():
-     print("la letra introducida es minuscula")
-else:
-    print("esto no es una letra")
+#
