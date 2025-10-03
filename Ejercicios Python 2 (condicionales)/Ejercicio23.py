195ff5ade86ec0383f9a95e61aa962251acef4cc
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-10-03 15:34:53 +0200

    python

--- Ejercicios Python 2 (condicionales)/Ejercicio23.py
+++ Ejercicios Python 2 (condicionales)/Ejercicio23.py
@@ -4,7 +4,7 @@ if var1<0 or var1>10:
     print("la nota es mayor de 10 o menor de 0")
 elif var1<5:
     print("has sacado un insuficiente")
-elif var1>=5 and var1<6.5:
+elif var1>=5 and var1<=6.5:
     print("has sacado un satisfactorio")
 elif var1>=6.5 and var1<8.5:
     print("has sacado un notable")
--- Ejercicios Python 2 (condicionales)/Ejercicio26.py
+++ Ejercicios Python 2 (condicionales)/Ejercicio26.py
@@ -1,1 +1,13 @@
 #Realiza un programa que, al introducir una letra por teclado, aparezca por pantalla si está o no en mayúscula. Utiliza dos IF’s que establezcan True o False a la condición.
+
+var1=(input("introduce una letra "))
+
+minuscula= False
+mayuscula=False
+
+if var1.isupper():
+    mayuscula=True and print("la letra introducida es mayuscula")
+elif var1.islower():
+    minuscula=True and print("la letra introducida es minuscula")
+else:
+    print("no has introducido una letra")
