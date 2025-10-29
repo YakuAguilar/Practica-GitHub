ec1f1e9054b49bbfc1c7006a01487dacb0304c85
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-10-29 09:51:10 +0100

    python

---
+++ Proyecto python/Proyecto.py
@@ -0,0 +1,46 @@
+
+print("                     Crea tu Password                       ")
+print("El password tiene que tener mas de 6 y menos de 8 caracteres")
+print("Posicion 1: Un numero major o igual a 1 o igual a 5         ")
+print("Posicion 2: Una letra minuscula                             ")
+print("Posicion 3: Una letra mayuscula                             ")
+print("Posicion 4: Uno de los siguientes simbolos:*,_,@            ")
+print("Posicion 5: Una letra minuscula                             ")
+print("Posicion 6: Un numero major o igual a 6 y mneor o igual a 9 ")
+print("Posicion 7: Uno de los siguinetes simbolos: &,/,#           ")
+print("Posicion 8: Un numero menor o igual que 5                   ")
+
+Password=str(input("introduce un Password"))
+print(Password[0])
+if [0] >=1 or 5:
+    
+
+
+#fraseerrrores= fraserrores  +  erro?
+
+print(Password[1])
+print(Password[2])
+print(Password[3])
+print(Password[4])
+print(Password[5])
+print(Password[6])
+print(Password[7])
+
+Longitudpassword=len(Password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
