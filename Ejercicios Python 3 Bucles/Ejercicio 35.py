9306291405a20d0d88d9e5adf49d7bbee4b3d6d9
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-11-07 15:47:29 +0100

    python

---
+++ Ejercicios Python 3 Bucles/Ejercicio 35.py
@@ -0,0 +1,8 @@
+nombre=(input("introduce tu nombre ")) 
+repeticiones=int(input("introduce la cantidad de veces que quieras que se repita tu nombre "))
+for j in range (repeticiones):
+    print(f"{nombre}")
+
+
+
+
