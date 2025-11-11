a5edb3f6774df0ad22f85e84e681f9eb749eca07
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-11-11 10:47:21 +0100

    Update Ejercicio 39.py

--- Ejercicios Python 3 Bucles/Ejercicio 39.py
+++ Ejercicios Python 3 Bucles/Ejercicio 39.py
@@ -1,5 +1,18 @@
 
-
-
+negativos=0
+positivos=0
+ceros=0
+numeroveces=int(input("introduce el numero de notas que quieres introducir "))
+for i in range (numeroveces):
+    nota= float(input("Introduce la nota : "))
+    if nota <0:
+        negativos=negativos+1
+        print(f"hay {negativos} numeros negativos")
+    elif nota>0:
+        positivo=positivo+1
+        print(f"hay {positivos} numeros positivos")
+    elif:
+        ceros=ceros+1
+        print(f"hay {ceros} ceros")
 
 
