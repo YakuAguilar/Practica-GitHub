69de0bd35022fbebe250ac1a7640d74a51a47b0b
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-11-11 10:22:13 +0100

    Update Ejercicio 37.py

---
+++ Ejercicios Python 3 Bucles/Ejercicio 37.py
@@ -0,0 +1,6 @@
+
+
+total=0
+numeroveces=int(input("introduce el numero de notas que quieres introducir "))
+for i in range(1, numeroveces + 1):
+   numeroveces = float(input(f"Introduce la nota {i}: "))
