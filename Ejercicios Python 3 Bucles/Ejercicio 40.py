e6c6d0312ba3d03bfbe037b276092cef904b1cb3
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-11-12 09:15:57 +0100

    Update Ejercicio 40.py

---
+++ Ejercicios Python 3 Bucles/Ejercicio 40.py
@@ -0,0 +1,13 @@
+
+pares=0
+impares=0 
+for i in range(0, 50): 
+    if i % 2 == 0: 
+        pares=pares+1 
+    else: 
+        impares=impares+1 
+        print(f" hay {pares} numeros pares") 
+        print(f"hay {impares} numeros impares") 
+
+
+
