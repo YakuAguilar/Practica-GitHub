cf3b0c8b80c89f7a04c53894995626c8e216d7ac
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-11-21 15:48:04 +0100

    python

---
+++ Ejercicios Python 4 bucles While/Ejercicio 50.py
@@ -0,0 +1,6 @@
+
+
+i=0
+while i<3:
+    print("Buenos dias")
+    i=i+1
---
+++ Ejercicios Python 4 bucles While/Ejercicio 51.py
@@ -0,0 +1,6 @@
+
+
+i=int(input("introduce las veces que quieres que se repita "))
+while i<i:
+    print("Buenos dias")
+    i=i+1
