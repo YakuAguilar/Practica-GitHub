eb2e29d4183f7fc289fb333bce02ee47b1c6ee02
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2025-12-12 15:33:18 +0100

    python

---
+++ Proyecto Password 2/Password 2.py
@@ -0,0 +1,11 @@
+#67. Realiza de nuevo el programa de Password (fase 2). El password debe tener las siguientes 
+#consideraciones:
+#Debe tener una longitud entre 6 y 8 caracteres.
+#Debe contener como mínimo:
+#2 números mayores o iguales que 1 y menor o igual que 5
+#2 letras minúsculas
+#1 letra mayúscula
+#2 símbolos (*, _, @, &,/,#)
+#1 número mayor o igual que 6 y menor o igual que 5
+
+
