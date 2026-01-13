1fcc2b78371352d3a82fc256169044f5821aaa4c
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-01-13 09:56:15 +0100

    Update Inicio.py

--- 2nd Trimestre/Inicio.py
+++ 2nd Trimestre/Inicio.py
@@ -1,4 +1,19 @@
-
 milista=[1,2,3,4,5,6]
+milistapor2=[]
+maximo=max(milista)
+minimo=min(milista)
+suma=sum(milista)
+print(milista)
+print("maximo:",maximo)
+print("minimo:",minimo)
+print("suma total:",suma)
 
 
+for x in range (len(milista)):
+    calculo=milista[x]*2
+    milistapor2.append(calculo)
+
+for x in milista:
+    milistapor2.append(x*2)
+
+print(milistapor2)
