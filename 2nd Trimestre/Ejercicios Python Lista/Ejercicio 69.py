4dc575f082365933052caec8498b59c7aa2915b3
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-01-14 09:28:27 +0100

    python

---
+++ 2nd Trimestre/Ejercicios Python Lista/Ejercicio 69.py
@@ -0,0 +1,21 @@
+
+
+
+milista=[]
+longitud=int(input("introduce la longitud de la lista "))
+
+for x in range (0,longitud):
+    numero=int(input("introduce un valor "))
+    milista.append(numero)
+milista.sort()
+print(milista)
+
+
+
+
+
+
+
+
+
+
