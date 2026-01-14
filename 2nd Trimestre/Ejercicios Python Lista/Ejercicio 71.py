7907b3ca23bb2d50bf22410a0b3a3d531613c856
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-01-14 09:51:53 +0100

    Update Ejercicio 71.py

---
+++ 2nd Trimestre/Ejercicios Python Lista/Ejercicio 71.py
@@ -0,0 +1,14 @@
+
+si=0
+no=1
+listaletras=[]
+
+print=input("introduce una letra entre comillas ")
+listaletras.append()
+seguir=input("quieres introducir otre letra? si/no")
+if seguir== 0:
+    print=input("introduce una letra")
+    listaletras.append()
+elif seguir== 1:
+    print(listaletras)
+
