580784e87cab2ac9bda5583f3d001f16f2304d30
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-01-14 08:59:18 +0100

    python

---
+++ 2nd Trimestre/1.py
@@ -0,0 +1,6 @@
+
+milista2=[]
+milista=[1,1,2,3,2,1,6,7,1]
+milista2=list(set(milista))
+#el list, es para que me devuelva una lista (viene entre []) y no un conjunto (vienen entre {})
+print(f"{milista2}")
---
+++ 2nd Trimestre/2.py
@@ -0,0 +1,17 @@
+
+
+listanumeros=[]
+listanonumeros=[]
+listatodo=[]
+frase=input("introduce valores separados por un guion ")
+#.split=para separar los digitos con un signo
+listatodo=frase.split("-")
+
+for x in range(len(listatodo)):
+    if listatodo[x].isnumeric():
+        listanumeros.append(listanumeros[x])
+    else:
+        listanumeros.append(listatodo[x])
+#append=
+print(listanumeros)
+print(sum(listanumeros))
