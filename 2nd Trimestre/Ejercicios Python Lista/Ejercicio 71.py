45c460a23d54f4491aa968c5bb11726d687be79e
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-01-20 10:42:27 +0100

    Update Ejercicio 71.py

--- 2nd Trimestre/Ejercicios Python Lista/Ejercicio 71.py
+++ 2nd Trimestre/Ejercicios Python Lista/Ejercicio 71.py
@@ -1,16 +1,15 @@
 
-si=()
-no=()
 listaletras=[]
-
-letra=input("introduce una letra entre comillas ")
+listaletras2=[]
+letra=input("introduce una letra ")
 listaletras.append(letra)
 #print(listaletras)
 seguir=input("quieres introducir otre letra? si/no: ")
-if seguir== si:
-    letra=input("introduce una letra entre comillas ")
+if seguir== "si":
+    letra=input("introduce una letra ")
     listaletras.append(letra)
-elif seguir== no:
-    print(listaletras)
-
+    seguir=input("quieres introducir otre letra? si/no: ")
+elif seguir== "no":
+    listaletras2=list(set(listaletras))
+    print(listaletras2)
  
