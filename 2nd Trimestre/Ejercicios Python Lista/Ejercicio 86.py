1e217aad48ea7be341f74fe2797379dbd432afa9
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-01-30 16:02:19 +0100

    Update Ejercicio 86.py

--- 2nd Trimestre/Ejercicios Python Lista/Ejercicio 86.py
+++ 2nd Trimestre/Ejercicios Python Lista/Ejercicio 86.py
@@ -3,11 +3,10 @@ numinicio=[]
 letrasDNI=["T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E"]
 numinicio=(input("introduce un numero de 8 digitos "))
 for x in range (len(numinicio)):
-    if len==8:
+    if (len(numinicio))==8:
         resto=numinicio%23
         print(resto)
-    else:
-        print("el numero introducido no tiene 8 digitos")
+
   
 
 
