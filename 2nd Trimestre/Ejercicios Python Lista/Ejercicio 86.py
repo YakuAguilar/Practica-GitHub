dea44251eb4abb11a5e13d5bcfbd4d85e7a20b5e
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-02-04 08:55:55 +0100

    Update Ejercicio 86.py

--- 2nd Trimestre/Ejercicios Python Lista/Ejercicio 86.py
+++ 2nd Trimestre/Ejercicios Python Lista/Ejercicio 86.py
@@ -2,11 +2,16 @@
 numinicio=[]
 letrasDNI=["T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E"]
 numinicio=(input("introduce un numero de 8 digitos "))
-for x in range (len(numinicio)):
-    if (len(numinicio))==8:
+for x in range (len(str(numinicio))):
+    if (len(str(numinicio))) ==8:
         resto=numinicio%23
-        print(resto)
+        if resto==0:
+            print()
 
-  
+
+dni_numero=(input("introduce un numero de 8 digitos "))
+tabla_letras="TRWAGMYFPDXBNJZSQVHLCKE"
+resto = dni_numero % 23
+return tabla_letras-[resto]
 
 
