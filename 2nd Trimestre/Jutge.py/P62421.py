fb317e38e055391273b8af66d5d6822953ac3987
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-02-10 10:24:46 +0100

    Update P62421.py

--- 2nd Trimestre/Jutge.py/P62421.py
+++ 2nd Trimestre/Jutge.py/P62421.py
@@ -1,7 +1,11 @@
 
 
-palabras=["hola", "adeu", "bye"]
-palabras2=palabras.copy()
-palabras2.reverse()
+
+palabras=[]
+for x in range (0,3):
+    caractespalabra=(input("introduce  3 palabras"))
+    palabras.append(caractespalabra)
+    palabras2=palabras.copy()
+    palabras2.reverse()
 print(palabras2)
 
