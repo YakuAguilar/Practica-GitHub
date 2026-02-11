7eaa3f5cb7370a9ad6ea00f87c819725fdc7204d
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-02-11 09:06:30 +0100

    Update P62421.py

--- 2nd Trimestre/Jutge.py/P62421.py
+++ 2nd Trimestre/Jutge.py/P62421.py
@@ -1,11 +1,8 @@
 
 
 
-palabras=[]
-for x in range (0,3):
-    caractespalabra=(input("introduce  3 palabras"))
-    palabras.append(caractespalabra)
-    palabras2=palabras.copy()
-    palabras2.reverse()
-print(palabras2)
+palabras=(input("introduce 3 palabras separadas por comas "))
+palabras=list(set(palabras))
+palabras.reverse()
+print(palabras)
 
