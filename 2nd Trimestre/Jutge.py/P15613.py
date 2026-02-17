aab1424b8bd04bab525b32c154d095728597bc0b
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-02-17 10:26:52 +0100

    Update P15613.py

---
+++ 2nd Trimestre/Jutge.py/P15613.py
@@ -0,0 +1,6 @@
+t = int(input())  
+if t > 30: print("it's hot") 
+elif t < 10: print("it's cold") 
+else: print("it's ok")
+if t >= 100: print("water would boil") 
+elif t <= 0: print("water would freeze")
