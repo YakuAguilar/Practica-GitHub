65950feb99c38424607a46b7ca4ea5decd6da004
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-02-18 09:48:41 +0100

    Update Ahorcado.py

---
+++ 2nd Trimestre/Ahorcado.py
@@ -0,0 +1,4 @@
+import random
+lista_palabrasecretas=["Patata", "Perro", "Ordenador", "Teclado", "Estuche", "Raton", "Movil", "Mochila", "Zapatos", "Rascacielos"]
+lista_partida=[]
+lista_ahorcado=[]
