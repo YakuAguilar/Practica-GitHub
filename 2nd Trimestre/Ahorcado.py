cd40ca179eb8ea19903d2f1542f3dd67a55518fc
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-02-24 09:57:51 +0100

    python

--- 2nd Trimestre/Ahorcado.py
+++ 2nd Trimestre/Ahorcado.py
@@ -1,4 +1,6 @@
 import random
 lista_palabrasecretas=["Patata", "Perro", "Ordenador", "Teclado", "Estuche", "Raton", "Movil", "Mochila", "Zapatos", "Rascacielos"]
 lista_partida=[]
-lista_ahorcado=[]
+lista_ahorcado=[]     
+print(int(input("introduce una letra")))
+lista_partida.append                          
--- 2nd Trimestre/ahorcado2.py
+++
@@ -1,4 +0,0 @@
-import random
-lista_palabrasecretas=["Patata", "Perro", "Ordenador", "Teclado", "Estuche", "Raton", "Movil", "Mochila", "Zapatos", "Rascacielos"]
-lista_partidas=[]
-lista_ahorcado=[]
