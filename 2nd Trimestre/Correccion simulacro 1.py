ee95bae2d4e9b226647103595e9c2532cf1906f9
YakuAguilar <yaku.aguilar@campus.xaloc.org>
2026-03-13 15:30:13 +0100

    Update Correccion simulacro 1.py

---
+++ 2nd Trimestre/Correccion simulacro 1.py
@@ -0,0 +1,17 @@
+#1.1 Utilizar las siguientes listas (con el mismo nombre) y otras variables que creas oportunas:
+#a.Lista_palabrasecreta
+#b.Lista_partida
+#c.Lista_ahorcado.
+#1.2 En Lista_palabrasecreta debe tener inicialmente 10 palabras ‘secretas’ asignadas. Utiliza
+#adecuadamente la librería Random para que se escoja una palabra al azar de la lista y sea la utilizada
+#en la partida.
+#1.3 En Lista_partida se almacenarán las letras acertadas respetando la colocación de las letras de la
+#palabra secreta. Inicialmente esta lista debe aparecer con tantos guiones “_” como longitud tenga la
+#palabra secreta.
+#Ejemplo:
+#Si la palabra secreta es: “amigo”, Lista_partida tendrá que tener establecidos 5 guiones
+#en blanco [ _ , _ , _ , _ , _]. Cada vez que se produzca un acierto deberá aparecer
+#reflejada la letra en la posición que corresponda
+#Ejemplo, si se introduce la letra “i”, en Lista_partida quedará [ _ , _ , i , _ , _ ]
+#1.4 En Lista_ahorcado deberá ir apareciendo la palabra A H O R C A D O a medida que se fallen letras.
+#1.5 La partida finaliza cuando se comenten 8 errores (a h o r c a d o) o se acierta la palabra secreta (debe aparecer un mensaje, por ejemplo, dando la enhorabuena). 
